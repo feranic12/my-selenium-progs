ac820e0653ad5b064fdd88a0f16a001283ac2dd6
Feranic <feranic@yandex.ru>
2019-02-21 17:15:48 +0300

    Done some refactoring in Military and Flat

--- FlatTest.py
+++ FlatTest.py
@@ -19,8 +19,8 @@ class FlatTest():
         self.driver.switch_to.frame("RESOLUTE_INSURANCE")
         self.fill_frame()
 
-    def fill_frame(self):
-        driver = self.driver
+    def first_page(self):
+        driver=self.driver
         flat_area=driver.find_element_by_id("area")
         flat_area.send_keys("100")
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[value *= 'МО']")))
@@ -32,54 +32,79 @@ class FlatTest():
         yes_rent.click()
         buy_standart=driver.find_element_by_css_selector("div#product-variant-1 div.btn-primary")
         buy_standart.click()
+
+    def additional_block(self):
+        driver=self.driver
         select_go = Select(driver.find_element_by_css_selector("select[register='pl_ss']"))
         select_go.select_by_index(1)
         select_strah = Select(driver.find_element_by_css_selector("select[register='ai_ss']"))
         select_strah.select_by_index(1)
-        begin_date=driver.find_element_by_id("beginDate")
-        begin_date.send_keys(get_begin_day(10))
+
+    def begin_date(self,days):
+        driver=self.driver
+        begin_date = driver.find_element_by_id("beginDate")
+        begin_date.send_keys(get_begin_day(days))
         begin_date.send_keys(Keys.ENTER)
-        region=driver.find_element_by_name("region")
+
+    def flat_address(self):
+        driver=self.driver
+        region = driver.find_element_by_name("region")
         region.click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[value*=\"415\"]")))
-        moscow=driver.find_element_by_css_selector("div[value*=\"415\"]")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[value*=\"415\"]")))
+        moscow = driver.find_element_by_css_selector("div[value*=\"415\"]")
         moscow.click()
-        street=driver.find_element_by_name("street")
+        street = driver.find_element_by_name("street")
         street.send_keys("Маяковского")
-        house=driver.find_element_by_name("house")
+        house = driver.find_element_by_name("house")
         house.send_keys("12")
-        corp=driver.find_element_by_name("corp")
+        corp = driver.find_element_by_name("corp")
         corp.send_keys("3")
-        building=driver.find_element_by_name("building")
+        building = driver.find_element_by_name("building")
         building.send_keys("4")
-        flat=driver.find_element_by_name("flat")
+        flat = driver.find_element_by_name("flat")
         flat.send_keys("3")
-        last_name=driver.find_element_by_name("lastName")
+
+    def insurer_info(self):
+        driver=self.driver
+        last_name = driver.find_element_by_name("lastName")
         last_name.send_keys("Петров")
-        first_name=driver.find_element_by_name("firstName")
+        first_name = driver.find_element_by_name("firstName")
         first_name.send_keys("Пётр")
-        middle_name=driver.find_element_by_name("middleName")
+        middle_name = driver.find_element_by_name("middleName")
         middle_name.send_keys("Петрович")
-        seria=driver.find_element_by_id("passportSeria")
+        seria = driver.find_element_by_id("passportSeria")
         seria.send_keys("1234")
-        number=driver.find_element_by_id("passportNumber")
+        number = driver.find_element_by_id("passportNumber")
         number.send_keys("123456")
-        passport_date=driver.find_element_by_id("passportDoi")
+        passport_date = driver.find_element_by_id("passportDoi")
         passport_date.send_keys("12102014")
         passport_date.send_keys(Keys.ENTER)
-        issued=driver.find_element_by_name("passportIssued")
+        issued = driver.find_element_by_name("passportIssued")
         issued.send_keys("ОВД")
-        birthdate=driver.find_element_by_id("dobA")
+        birthdate = driver.find_element_by_id("dobA")
         birthdate.send_keys("21101978")
         birthdate.send_keys(Keys.ENTER)
-        phone=driver.find_element_by_name("phone")
+        phone = driver.find_element_by_name("phone")
         phone.send_keys("+7(234)123-12-12")
-        email1=driver.find_element_by_name("email1")
+        email1 = driver.find_element_by_name("email1")
         email1.send_keys("knikitin@avinfors.ru")
-        email2=driver.find_element_by_name("email2")
+        email2 = driver.find_element_by_name("email2")
         email2.send_keys("knikitin@avinfors.ru")
-        agree=driver.find_element_by_name("agree")
+
+    def agree(self):
+        driver=self.driver
+        agree = driver.find_element_by_name("agree")
         agree.click()
+
+    def fill_frame(self):
+        driver = self.driver
+        self.first_page()
+        self.additional_block()
+        self.begin_date(10)
+        self.flat_address()
+        self.insurer_info()
+        self.agree()
+
 flat_test = FlatTest()
 
 
--- MilitaryTest.py
+++ MilitaryTest.py
@@ -17,113 +17,109 @@ class Military():
         self.driver.switch_to.frame(0)
         self.fill_frame()
 
-    def fill_frame(self):
+    def choose_place(self):
         driver=self.driver
-
-        choose_place=driver.find_elements_by_css_selector("div#calculation button")[0]
+        choose_place = driver.find_elements_by_css_selector("div#calculation button")[0]
         choose_place.click()
-        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[data-raw *= \"ВТБ (ПАО)\"]")))
-        vtb24=driver.find_element_by_css_selector("div[data-raw *= \"ВТБ (ПАО)\"]")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[data-raw *= \"ВТБ (ПАО)\"]")))
+        vtb24 = driver.find_element_by_css_selector("div[data-raw *= \"ВТБ (ПАО)\"]")
         vtb24.click()
-
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[data-raw *= \"Москва\"]")))
         moscow = driver.find_element_by_css_selector("div[data-raw *= \"Москва\"]")
         moscow.click()
-
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[data-raw *= \"Дмитровский\"]")))
         habarovsk = driver.find_element_by_css_selector("div[data-raw *= \"Дмитровский\"]")
         habarovsk.click()
 
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.contractData.flat.creditSize\"]")))
-        credit=driver.find_element_by_css_selector("input[data-path=\"content.contractData.flat.creditSize\"]")
+    def conditions(self):
+        driver=self.driver
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.contractData.flat.creditSize\"]")))
+        credit = driver.find_element_by_css_selector("input[data-path=\"content.contractData.flat.creditSize\"]")
         credit.send_keys("100000")
-
-        #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.contractData.flat.rate\"]")))
+        # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.contractData.flat.rate\"]")))
         credit = driver.find_element_by_css_selector("input[data-path=\"content.contractData.flat.rate\"]")
         credit.send_keys("20")
-
-        #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.contractData.flat.countMonth\"]")))
+        # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.contractData.flat.countMonth\"]")))
         count_months = driver.find_element_by_css_selector("input[data-path=\"content.contractData.countMonth\"]")
         count_months.send_keys("30")
-
-        buttons=driver.find_elements_by_class_name("sectionInsurersButton")
+        buttons = driver.find_elements_by_class_name("sectionInsurersButton")
         buttons[0].click()
         buttons[1].click()
 
+    def flat_address(self):
+        driver=self.driver
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.object.address\"]")))
-        address=driver.find_element_by_css_selector("input[data-path=\"content.object.address\"]")
+        address = driver.find_element_by_css_selector("input[data-path=\"content.object.address\"]")
         address.send_keys("Москва")
         time.sleep(1)
         address.send_keys(Keys.ARROW_DOWN)
         time.sleep(1)
         address.send_keys(Keys.ENTER)
-
         address1 = driver.find_element_by_css_selector("input[data-path=\"content.object.street\"]")
         address1.send_keys("Маяковская 34 17")
-
-        button_3=driver.find_element_by_css_selector("button[data-for=\"step3\"]")
+        button_3 = driver.find_element_by_css_selector("button[data-for=\"step3\"]")
         button_3.click()
 
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.insuredPerson.lastName\"]")))
+    def insurer_info(self):
+        driver=self.driver
+        WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[data-path=\"content.insuredPerson.lastName\"]")))
         last_name = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.lastName\"]")
         last_name.send_keys("Петров")
-
         first_name = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.firstName\"]")
         first_name.send_keys("Пётр")
-
         middle_name = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.middleName\"]")
         middle_name.send_keys("Петрович")
-
         dob = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.dob\"]")
         dob.click()
         dob.send_keys("18021980")
-
         body = driver.find_element_by_tag_name("body")
         body.click()
-
         time.sleep(2)
         male = driver.find_element_by_css_selector("button[value*=\"Мужской\"]")
         male.click()
-
         passport = driver.find_element_by_css_selector("input[data-type=\"passport\"]")
         passport.click()
         passport.send_keys("1234 123456")
-
         doi = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.passport.doi\"]")
         doi.click()
         doi.send_keys("18022014")
         body.click()
-
         issued = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.passport.issued\"]")
         issued.send_keys("ОВД")
-
-        phone=driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.phone\"]")
+        phone = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.phone\"]")
         phone.click()
         phone.send_keys("1112223344")
-
         email1 = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.email\"]")
         email1.send_keys("knikitin@avinfors.ru")
-
-        email2=driver.find_element_by_css_selector("input[equal=\"content.insuredPerson.email\"]")
+        email2 = driver.find_element_by_css_selector("input[equal=\"content.insuredPerson.email\"]")
         email2.send_keys("knikitin@avinfors.ru")
-
         insurer_address = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.address\"]")
         insurer_address.send_keys("Москва")
         time.sleep(1)
         insurer_address.send_keys(Keys.ARROW_DOWN)
         time.sleep(1)
         insurer_address.send_keys(Keys.ENTER)
-
         insurer_address1 = driver.find_element_by_css_selector("input[data-path=\"content.insuredPerson.street\"]")
         insurer_address1.send_keys("Маяковская 34 17")
-
-        button_4=driver.find_element_by_css_selector("button[data-for=\"step4\"]")
+        button_4 = driver.find_element_by_css_selector("button[data-for=\"step4\"]")
         button_4.click()
 
+    def agree(self):
+        driver=self.driver
         time.sleep(2)
-        agree1=driver.find_element_by_id("checkbox1")
+        agree1 = driver.find_element_by_id("checkbox1")
         agree1.click()
-
-        agree2=driver.find_element_by_id("checkbox2")
+        agree2 = driver.find_element_by_id("checkbox2")
         agree2.click()
+
+    def fill_frame(self):
+        driver=self.driver
+        self.choose_place()
+        self.conditions()
+        self.flat_address()
+        self.insurer_info()
+        self.agree()
+
 military=Military()
