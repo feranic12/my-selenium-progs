7e654c1ac82cf05fc332ea8a8624fa5bb60351da
Kirill Nikitin <feranic@yandex.ru>
2019-03-28 22:37:49 +0300

    Добавил возможность выбирать открываемый УРЛ из шести вариантов.

--- Tests/FlatTest.py
+++ Tests/FlatTest.py
@@ -6,7 +6,12 @@ from Fixtures.FlatFixture import FlatFixture
 @pytest.fixture
 def fix(request):
     browser = request.config.getoption("--browser")
-    target = r"https://testpartner.vtbins.ru/b2c/flat/test-main.html"
+    target = {"1": r"https://testpartner.vtbins.ru/b2c/flat/test-main.html",
+             "2": r"https://testpartner.vtbins.ru/b2c/flat/test-main.html?p=rad",
+             "3": r"https://testpartner.vtbins.ru/b2c/flat/test-main.html?client=123",
+             "4": r"https://testpartner.vtbins.ru/b2c/flat/test-main.html?p=rad&client=123",
+             "5": r"https://vtbins.ru/b2c/flat/test-main.html",
+             "6": r"https://2.vtbins.ru/b2c/flat/test-main.html"}[request.config.getoption("--target")]
     fixture = FlatFixture(browser, target)
     return fixture
 
