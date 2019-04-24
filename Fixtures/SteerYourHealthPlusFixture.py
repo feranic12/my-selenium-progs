863f414e04387f0ed2a27e5b374419abc23dbe03
Feranic <feranic@yandex.ru>
2019-04-24 17:20:38 +0300

    Доделал УправляйЗдоровьем+

--- Fixtures/SteerYourHealthPlusFixture.py
+++ Fixtures/SteerYourHealthPlusFixture.py
@@ -27,7 +27,7 @@ class SteerYourHealthPlusFixture:
         age17 = driver.find_element_by_css_selector("label[for=\"UZSIP1-17\"]")
         age17.click()
         # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.btnBuy[btn-id=\"standart\"]")))
-        time.sleep(5)
+        time.sleep(4)
         standart = driver.find_element_by_css_selector("div.btnBuy[btn-id=\"standart\"]")
         standart.click()
 
@@ -58,6 +58,51 @@ class SteerYourHealthPlusFixture:
         button_next = driver.find_element_by_css_selector("button[data-next=\"step3\"]")
         button_next.click()
 
+    def insurer_info(self):
+        driver = self.driver
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[field-id=\"insurerAdultSection_surname\"]")))
+        last_name = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_surname\"]")
+        last_name.send_keys("Петров")
+        first_name = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_name\"]")
+        first_name.send_keys("Пётр")
+        middle_name = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_secondName\"]")
+        middle_name.send_keys("Петрович")
+        dob = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_birthdate\"]")
+        dob.click()
+        dob.send_keys("01011990"+Keys.ENTER)
+        body = driver.find_element_by_tag_name("body")
+        body.click()
+        time.sleep(1)
+        male = driver.find_element_by_css_selector("button[field-id=\"insurerAdultSection_male\"]")
+        male.click()
+        passport = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_passportNumber\"]")
+        passport.click()
+        passport.send_keys("111111222222")
+        issue_date = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_issueDate\"]")
+        issue_date.click()
+        issue_date.send_keys("01012012" + Keys.ENTER)
+        issue_organ = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_issueOrgan\"]")
+        issue_organ.send_keys("ОВД")
+        phone = driver.find_element_by_css_selector("input[field-id=\"insurerAdultSection_mobile\"]")
+        phone.click()
+        phone.send_keys("1231231212")
+        list = driver.find_elements_by_css_selector("input[field-id=\"insurerAdultSection_email\"]")
+        email = list[0]
+        email.send_keys("knikitin@avinfors.ru")
+        email1 = list[1]
+        email1.send_keys("knikitin@avinfors.ru")
+        button_next = driver.find_element_by_css_selector("button[data-next=\"step5\"]")
+        button_next.click()
+
+    def agree(self):
+        driver = self.driver
+        #WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div#checkboxBlock>div")))
+        time.sleep(1)
+        agree = driver.find_element_by_css_selector("div#checkboxBlock>div")
+        agree.click()
+
     def fill_frame(self):
         self.conditions()
         self.insured_child()
+        self.insurer_info()
+        self.agree()
