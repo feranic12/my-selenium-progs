1cfb4eaf638b715202dbb45ce580cbac606b9441
Kirill Nikitin <feranic@yandex.ru>
2019-05-20 22:33:43 +0300

    Убрал ненужный импорт pyperclip в BaseFixture

--- Fixtures/BaseFixture.py
+++ Fixtures/BaseFixture.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import pyperclip
 import time
 
 class BaseFixture:
